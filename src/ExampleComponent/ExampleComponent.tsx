bb17cb0388132afffa9acf0939c920ed962255e2
Amir Tugendhaft <tugi@melio.com>
2024-11-05 18:11:32 +0200

    Add eslint and prettier

--- src/ExampleComponent/ExampleComponent.tsx
+++ src/ExampleComponent/ExampleComponent.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
-export const ExampleComponent = (
-  { backgroundColor = '#d9ccff', content = 'Hello, Penny!' }) => {
-    return <span style={{ fontFamily: 'sans-serif', backgroundColor, padding: '12px', borderRadius: '4px' }}>{content}</span>
-}
+export const ExampleComponent = ({
+  backgroundColor = '#d9ccff',
+  content = 'Hello, Penny!',
+}: {
+  backgroundColor: string,
+  content: string,
+}) => {
+  return (
+    <span
+      style={{
+        fontFamily: 'sans-serif',
+        backgroundColor,
+        padding: '12px',
+        borderRadius: '4px',
+      }}
+    >
+      {content}
+    </span>
+  );
+};
